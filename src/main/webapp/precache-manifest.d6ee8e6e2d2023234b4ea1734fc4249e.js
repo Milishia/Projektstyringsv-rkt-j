629169b962a4b5580efef3e48e4b4b6794f663be
futte111 <ntthyssen@gmail.com>
2019-10-01 12:22:54 +0200

    nothing

---
+++ src/main/webapp/precache-manifest.d6ee8e6e2d2023234b4ea1734fc4249e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b4c97c4411459cb901c0537bfb3ed7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82d84eca247ec9fdbb4b",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "55dbadc3dbd3795c6b5d",
+    "url": "/static/css/main.d68031a4.chunk.css"
+  },
+  {
+    "revision": "82d84eca247ec9fdbb4b",
+    "url": "/static/js/2.ed0a5f5a.chunk.js"
+  },
+  {
+    "revision": "55dbadc3dbd3795c6b5d",
+    "url": "/static/js/main.38404b5f.chunk.js"
+  },
+  {
+    "revision": "3eb89e8c880c493c93ea",
+    "url": "/static/js/runtime-main.0b761a9c.js"
+  }
+]);
