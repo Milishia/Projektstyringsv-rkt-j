2b032890c371384b962b2340de84bfea107e070c
futte111 <ntthyssen@gmail.com>
2019-10-22 11:05:33 +0200

    rollback to working commit

---
+++ src/main/webapp/precache-manifest.0e7fa4a9c808994a0155039cf376a22e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65f43040875b5116d77738e74aedb96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5d700290fd0364c9ab5",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "3defad606301704bc641",
+    "url": "/static/css/main.d68031a4.chunk.css"
+  },
+  {
+    "revision": "a5d700290fd0364c9ab5",
+    "url": "/static/js/2.c1be0509.chunk.js"
+  },
+  {
+    "revision": "3defad606301704bc641",
+    "url": "/static/js/main.0509622f.chunk.js"
+  },
+  {
+    "revision": "3eb89e8c880c493c93ea",
+    "url": "/static/js/runtime-main.0b761a9c.js"
+  }
+]);
---
+++ src/main/webapp/precache-manifest.cd8f086f8a232af81a2f6882a7cf6619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e7578fb6f6f3ca8536373450f38e6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f43f05db769f9cc9ec5",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "212efcd3cd8e1e9b2d0d",
+    "url": "/static/css/main.d68031a4.chunk.css"
+  },
+  {
+    "revision": "7f43f05db769f9cc9ec5",
+    "url": "/static/js/2.7cf859b5.chunk.js"
+  },
+  {
+    "revision": "212efcd3cd8e1e9b2d0d",
+    "url": "/static/js/main.a16d774a.chunk.js"
+  },
+  {
+    "revision": "3eb89e8c880c493c93ea",
+    "url": "/static/js/runtime-main.0b761a9c.js"
+  }
+]);
