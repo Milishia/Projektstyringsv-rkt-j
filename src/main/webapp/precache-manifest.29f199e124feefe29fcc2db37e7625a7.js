815e89d12ea5676efedda925752104e8b404d976
futte111 <ntthyssen@gmail.com>
2019-10-29 11:43:37 +0100

    Maven packge

---
+++ src/main/webapp/precache-manifest.29f199e124feefe29fcc2db37e7625a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef045007330d5517b0fa992e3ad115c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b20eea61a6487823af70",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "031f001ec0265f0ffae6",
+    "url": "/static/css/main.d68031a4.chunk.css"
+  },
+  {
+    "revision": "b20eea61a6487823af70",
+    "url": "/static/js/2.725c8b33.chunk.js"
+  },
+  {
+    "revision": "031f001ec0265f0ffae6",
+    "url": "/static/js/main.40de1e34.chunk.js"
+  },
+  {
+    "revision": "3eb89e8c880c493c93ea",
+    "url": "/static/js/runtime-main.0b761a9c.js"
+  }
+]);
