1713ea0547a44a337439d5341a7047a841110789
Daniel Greibe <danielgreibe@gmail.com>
2019-11-06 11:31:19 +0100

    @GET and @POST works in the database. Gets mapped to a Stakeholder object. Not implemented in the WebApp yet.

---
+++ src/main/java/Stakeholder.java
@@ -0,0 +1,13 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Stakeholder {
+    private String name;
+    private String email;
+}
