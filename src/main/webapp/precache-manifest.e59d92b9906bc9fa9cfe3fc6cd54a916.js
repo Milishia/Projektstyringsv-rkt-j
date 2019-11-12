490df333b9a053d04bac16e2e4e2230be53fe68b
Daniel Greibe <Danielgreibe@gmail.com>
2019-11-12 10:50:25 +0100

    Everything didn't get committed?

---
+++ src/main/webapp/precache-manifest.e59d92b9906bc9fa9cfe3fc6cd54a916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e71990640c6250a7d6e8c3c4aafee1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b7c7a236fecd7423bba",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7ef76ae69d707b335006",
+    "url": "/static/css/main.c30ef553.chunk.css"
+  },
+  {
+    "revision": "9b7c7a236fecd7423bba",
+    "url": "/static/js/2.3c2c91f7.chunk.js"
+  },
+  {
+    "revision": "7ef76ae69d707b335006",
+    "url": "/static/js/main.78e98fcc.chunk.js"
+  },
+  {
+    "revision": "3eb89e8c880c493c93ea",
+    "url": "/static/js/runtime-main.0b761a9c.js"
+  }
+]);
