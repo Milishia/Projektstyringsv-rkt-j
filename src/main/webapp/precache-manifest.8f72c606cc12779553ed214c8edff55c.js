1db8040a60fe3ae1f9fcf1761133510c6ca0d0b8
caro <carlankjaer@gmail.com>
2019-12-03 12:44:17 +0100

    Tilføjet links i navbar

---
+++ src/main/webapp/precache-manifest.8f72c606cc12779553ed214c8edff55c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145ecadd3421b445d43c40442f81f766",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef69fbf7e722909c061e",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f4ff6bc2a850c26a1f4e",
+    "url": "/static/css/main.d68031a4.chunk.css"
+  },
+  {
+    "revision": "ef69fbf7e722909c061e",
+    "url": "/static/js/2.afb6d80b.chunk.js"
+  },
+  {
+    "revision": "f4ff6bc2a850c26a1f4e",
+    "url": "/static/js/main.62275399.chunk.js"
+  },
+  {
+    "revision": "3eb89e8c880c493c93ea",
+    "url": "/static/js/runtime-main.0b761a9c.js"
+  }
+]);
