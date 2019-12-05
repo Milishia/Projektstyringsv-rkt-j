86b963b95882cd5b13c8cc314951075287e759e9
futte111 <ntthyssen@gmail.com>
2019-12-05 15:39:07 +0100

    mvn packge

--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1c260668d3f152aa9269bfd851b2274.js"
+  "/precache-manifest.c782045c7424291e9914c923347f9c07.js"
 );
 
 self.addEventListener('message', (event) => {
